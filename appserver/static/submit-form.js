847ee0aaf4730890b2b893e721124fc7dfda4944
DevAlphaKilo <DevAlphaKilo@gmail.com>
2019-09-30 15:17:31 -0400

    add stylesheet and js
    Signed-off-by: DevAlphaKilo <DevAlphaKilo@gmail.com>

---
+++ appserver/static/submit-form.js
@@ -0,0 +1,6 @@
+require([ "jquery", "splunkjs/mvc/simplexml/ready!"],
+          function($){
+             $("#submit_button").on("click", function (){
+                  alert("Test");
+             });
+          });
