a7cf773854c6c3e0fa53809ddd850cbe75a0deb6
DevAlphaKilo <DevAlphaKilo@gmail.com>
2019-11-07 17:57:57 -0500

    updated to work
    Signed-off-by: DevAlphaKilo <DevAlphaKilo@gmail.com>

--- appserver/static/autofill-date-today.js
+++ appserver/static/autofill-date-today.js
@@ -1,13 +1,13 @@
-require([   "jquery",
-            "splunkjs/mvc/simplexml/ready!"
+require(["jquery","splunkjs/mvc/simplexml/ready!"],
 function($){
-    var
     var now = new Date();
     var date = (now.getMonth() + 1) + "/" + now.getDate() + "/" + now.getFullYear()
     //$("input[role='textbox'").each(function( index ) {
     //    //$(this).attr("value", date)
     //    $(this).val(date)
     //});
-	var datePurchase = mvc.Compoents.get("purchase_date")
+	var datePurchased = mvc.Compoents.get("purchase_date")
 	var dateSealed = mvc.Compoents.get("sealed_date")
+	console.log(datePurchased);
+	console.log(dateSealed);
 });
