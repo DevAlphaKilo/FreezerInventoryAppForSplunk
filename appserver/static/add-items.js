109e84bf0a40af7030633498fc1d1e5ed4ec2804
DevAlphaKilo <DevAlphaKilo@gmail.com>
2019-11-13 11:46:10 -0500

    updated to post to items endpoint
    Signed-off-by: DevAlphaKilo <DevAlphaKilo@gmail.com>

--- appserver/static/add-items.js
+++ appserver/static/add-items.js
@@ -1,51 +1,3 @@
-
-var splunkWebHttp = new splunkjs.SplunkWebHttp();
-var service = new splunkjs.Service(splunkWebHttp);
-var indexes = service.indexes();
-function insertData(indexName, data, host, sourcetype) {
-    // first get the index to use 
-    indexes.fetch(function(err, indexes) {
-        var myIndex = indexes.item(indexName);
-        if (myIndex) {
-            // console.log("Found " + myIndex.name + " index");
-        } else {
-            // console.log("Error!  Could not find index named " + indexName);
-            return null;
-        }
-        // ******  Need to loop through JSON by items or text by line
-        // Now loop through the data and insert each row or record
-        //  lookup the sourcetype from the samples array and find the parse_method  (line | array)
-        var parse_method = "line";
-        // console.log("parsing as line");
-        fileLines = data.split("\n")
-        for (var i = 0; i < fileLines.length; i++) {
-            // Submit an event to the index
-            myIndex.submitEvent(fileLines[i], { host: host, sourcetype: sourcetype },
-                function(err, result, myIndex) {
-                    // console.log("Submitted event: ", result, "sourcetype:", sourcetype, "host:", host);
-                });
-        }
-    });
-}
-
-//function insertData() {
-//	$.ajax({
-//        url: 'https://' + window.location.hostname + ':8088/services/collector/event',
-//        type: 'POST',
-//        async: false,
-//        data: JSON.stringify({ "event": record }),
-//        beforeSend: function(xhr) { xhr.setRequestHeader('Authorization', 'Splunk 1dg3235-ca51-439c-b816-64cf1963b251'); },
-//        success: function(data, textStatus, xhr) {
-//            $("#results").html("Success! <a href=\"" + window.location.origin + "/app/search/search?q=search%20index=_internal%20sourcetype=testdata\" target=\"_blank\">Launch Search for Data</a>")
-//        },
-//        error: function(xhr, textStatus, error) {
-//            console.error("Error!", error);
-//            $("#results").html("<p style=\"margin-top: 10px;\">Failure! Check the console for details... but probably it's because of a certificate error, unless you happen to have a valid certificate on your Splunk environment. The next leading cause of a failure here would be that there is a load balancer or firewall that's preventing access to that port.</p>")
-//        }
-//    })
-//
-//}
-
 function resetForm(mvc) {
     // reset dropdowns after posting events
     $("div[class='input input-dropdown'").each(function( index ) {
@@ -57,8 +9,8 @@ function resetForm(mvc) {
     mvc.Components.getInstance('default').unset("input_complete");
 }
 
-require(["jquery","splunkjs/mvc","splunkjs/mvc/simplexml/ready!"],
-function($, mvc){
+require(["jquery","splunkjs/mvc",'splunk.util',"splunkjs/mvc/simplexml/ready!"],
+function($, mvc, splunkUtil){
     // clear forms when updated
     $("div[class='input input-dropdown'").each(function( index ) {
         //console.log(this.id);
@@ -92,18 +44,28 @@ function($, mvc){
         $.each(tableResults,function( result_index ) {
             // each result item 
             this["action"] = "added"
-			this["status"] = "available"
+            this["status"] = "available"
+            delete this['pack_id'];
             data = JSON.stringify(this);
             console.log(data);
-            insertData("test", data, "FreezerInventory", "freezer:item");
+
+            var rest_url = splunkUtil.make_url('/splunkd/__raw/services/freezer_inventory/items');
+            var post_data = {
+                action            : 'add_item',
+                item_data : data,
+            };
+            $.post( rest_url, post_data, function(data, status) {
+                console.log(data);
+                console.log(status);
+            }, "json");
         });
-		var modal = ''+
+        var modal = ''+
 '<div class="modal fade" id="post_success">' +
 '  <div class="modal-dialog model-sm">' +
 '    <div class="modal-content">' +
 '      <div class="modal-header">' +
 '        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' +
-'        <h4 class="modal-title">Post Successful</h4>' +
+'        <h4 class="modal-title">Item(s) Added</h4>' +
 '      </div>' +
 '      <div class="modal-body">' +
 '        <p>Your items have been added to your freezer inventory.</p>' +
@@ -118,5 +80,5 @@ function($, mvc){
         $('#post_success').modal('show');
         resetForm(mvc);
     });
-	$("#reset_button").on("click", function (){ resetForm(mvc); });
+    $("#reset_button").on("click", function (){ resetForm(mvc); });
 });
