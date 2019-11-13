af34a2782998b75659c5accf67c83f8a7afe3034
DevAlphaKilo <DevAlphaKilo@gmail.com>
2019-11-12 20:44:56 -0500

    added rest endpoint get_items and logger
    Signed-off-by: DevAlphaKilo <DevAlphaKilo@gmail.com>

--- bin/items_rest_endpoint.py
+++ bin/items_rest_endpoint.py
@@ -1,16 +1,22 @@
-
+import os
+import sys
+import json
+import urllib
+import operator
+import httplib
 
 import splunk
 import splunk.appserver.mrsparkle.lib.util as util
+import splunk.entity as entity
 import splunk.rest as rest
 
-from EventHandler import *
-
-dir = os.path.join(util.get_apps_dir(), 'alert_manager', 'bin', 'lib')
+dir = os.path.join(util.get_apps_dir(), 'FreezerInventoryAppForSplunk', 'bin', 'lib')
 if not dir in sys.path:
     sys.path.append(dir)
 	
-logger = setupLogger('rest_handler')
+from FreezerInventoryLogger import *
+   
+logger = setupLogger('freezer_inventory')
 
 if sys.platform == "win32":
     import msvcrt
@@ -25,7 +31,7 @@ class ItemsEndpoint(PersistentServerConnectionApplication):
     def __init__(self, command_line, command_arg):
         PersistentServerConnectionApplication.__init__(self)
 
-    def endpoint(self, args):
+    def handle(self, args):
         logger.debug("START endpoint()")
         logger.debug('ARGS: %s', args)
 
@@ -47,9 +53,8 @@ class ItemsEndpoint(PersistentServerConnectionApplication):
             msg = 'Unknown exception: %s' % e
             logger.exception(msg)
             return self.response(msg, httplib.INTERNAL_SERVER_ERROR)
-			
 
-    def endpoint_get(self, args):
+    def handle_get(self, args):
         logger.debug('GET ARGS %s', json.dumps(args))
 
         query_params = dict(args.get('query', []))
@@ -59,7 +64,6 @@ class ItemsEndpoint(PersistentServerConnectionApplication):
             user = args["session"]["user"]
         except KeyError:
             return self.response("Failed to obtain auth token", httplib.UNAUTHORIZED)
-
 
         required = ['action']
         missing = [r for r in required if r not in query_params]
@@ -74,8 +78,8 @@ class ItemsEndpoint(PersistentServerConnectionApplication):
             logger.exception(msg)
             return self.response(msg, httplib.BAD_REQUEST)
 
-    def endpoint_post(self, args):
-        logger.debug('POST ARGS %s', json.dumps(args))
+    def handle_post(self, args):
+        #logger.debug('POST ARGS %s', json.dumps(args))
 
         post_data = dict(args.get('form', []))
 
@@ -96,7 +100,7 @@ class ItemsEndpoint(PersistentServerConnectionApplication):
             return operator.methodcaller(action, sessionKey, user, post_data)(self)
         else:
             msg = 'Invalid action: action="{}"'.format(action)
-            logger.exception(msg)
+            #logger.exception(msg)
             return self.response(msg, httplib.BAD_REQUEST)
 
     @staticmethod
@@ -112,28 +116,52 @@ class ItemsEndpoint(PersistentServerConnectionApplication):
             }
         return {'status': status, 'payload': payload}
 
-    def _add_item(self, sessionKey, user, post_data):
-        logger.debug("START _add_item()")
-		required = ['item_data']
+    def _get_items(self, sessionKey, query_params):
+        logger.debug("START _list_item_details()")
+
+        splunk.setDefault('sessionKey', sessionKey)
+        
+        items_uri = '/servicesNS/nobody/FreezerInventoryAppForSplunk/storage/collections/data/items?output_mode=json'
+        
+        # Get item json
+        serverResponse, serverContent = rest.simpleRequest(items_uri, sessionKey=sessionKey, method='GET')
+        logger.debug("items: %s" % serverContent)
+        items = json.loads(serverContent)
+
+        return self.response(items, httplib.OK)
+        
+    def _post_item(self, sessionKey, query_params, post_data):
+        logger.debug("START _post_item()")
+        required = ['item_data']
         missing = [r for r in required if r not in post_data]
         if missing:
             return self.response("Missing required arguments: %s" % missing, httplib.BAD_REQUEST)
 
-        incident_data = post_data.pop('item_data')
+        item_data = post_data.pop('item_data')
 
         splunk.setDefault('sessionKey', sessionKey)
 
-        eh = EventHandler(sessionKey = sessionKey)
-		
-		config = {}
-        config['index'] = 'test'
-		
-        restconfig = entity.getEntities('configs/freezer_inventory', count=-1, sessionKey=sessionKey)
-        if len(restconfig) > 0:
-            if 'index' in restconfig['settings']:
-                config['index'] = restconfig['settings']['index']
-		
-        logger.debug("Global settings: %s" % config)
-		
-		# Parse the JSON
+        #eh = EventHandler(sessionKey = sessionKey)
+        
+        #config = {}
+        #config['index'] = 'test'
+        #
+        #restconfig = entity.getEntities('configs/freezer_inventory', count=-1, sessionKey=sessionKey)
+        #if len(restconfig) > 0:
+        #    if 'index' in restconfig['settings']:
+        #        config['index'] = restconfig['settings']['index']
+        #
+        #logger.debug("Global settings: %s" % config)
+        
+        # Parse the JSON
         item_data = json.loads(item_data)
+        
+        items_uri = '/servicesNS/nobody/FreezerInventoryAppForSplunk/storage/collections/data/items'
+        
+        # Get incident json
+        serverResponse, serverContent = rest.simpleRequest(item_uri, sessionKey=sessionKey, jsonargs=item_data, method='POST')
+        logger.debug("items: %s" % serverContent)
+        items = json.loads(serverContent)
+
+        return self.response(items, httplib.OK)
+
---
+++ bin/lib/FreezerInventoryLogger.py
@@ -0,0 +1,43 @@
+import os
+import logging
+from logging.config import fileConfig
+import ConfigParser
+import splunk.appserver.mrsparkle.lib.util as util
+
+def setupLogger(logger='freezer_inventory'):
+
+	# logger
+	fileName = 'freezer_inventory.log'
+	if logger != 'freezer_inventory':
+		fileName = 'freezer_inventory%s.log' % logger
+		logger = 'freezer_inventory%s' % logger
+
+	# Get loglevel from config file
+	local = os.path.join(util.get_apps_dir(), "FreezerInventoryAppForSplunk", "local", "freezer_inventory.conf")
+	default = os.path.join(util.get_apps_dir(), "FreezerInventoryAppForSplunk", "default", "freezer_inventory.conf")
+
+	config = ConfigParser.ConfigParser()
+
+	try:
+		config.read(local)
+		rootLevel = config.get('logging', 'rootLevel')
+	except:
+		config.read(default)
+		rootLevel = config.get('logging', 'rootLevel')
+
+	try:
+		logLevel = config.get('logging', 'logger.%s' % logger)
+	except:
+		logLevel = rootLevel
+
+	# Setup logger
+	log = logging.getLogger(logger)
+	lf = os.path.join(os.environ.get('SPLUNK_HOME'), "var", "log", "splunk", fileName)
+	fh = logging.handlers.RotatingFileHandler(lf, maxBytes=25000000, backupCount=5)
+	formatter = logging.Formatter('%(asctime)s %(levelname)-6s pid="%(process)s" logger="%(name)s" message="%(message)s" (%(filename)s:%(lineno)s)')
+	fh.setFormatter(formatter)
+	log.addHandler(fh)
+	level = logging.getLevelName(logLevel)
+	log.setLevel(level)
+
+	return log
